812ce27cdbfcb22c6b9b0fd75b5fcad313071b7d
Ryan Huber <rhuber@gmail.com>
2018-07-31 11:08:03 +0000

    whole numbers

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -694,7 +694,7 @@
  *    (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 0  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -45.2  // Y offset: -front +behind [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -45  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
