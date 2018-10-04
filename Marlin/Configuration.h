a68a23e59b506392902f9b45334a87f23e8c089f
Ryan Huber <rhuber@gmail.com>
2018-10-04 14:03:07 +0000

    yay firmwarez

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -575,8 +575,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 8.0
-#define DEFAULT_YJERK                 8.0
+#define DEFAULT_XJERK                 15.0
+#define DEFAULT_YJERK                 15.0
 #define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  1.0
 
